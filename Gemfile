4c7234f298f17acf1af6a67cf18e85925e3aa0f5
Lars Knoke <larz.knoke@gmail.com>
2018-05-23 20:30:32 +0200

    releaseV1

--- Gemfile
+++ Gemfile
@@ -2,4 +2,7 @@ source "https://rubygems.org"
 
 group :jekyll_plugins do
   gem 'jekyll-haml'
+  gem 'jekyll-autoprefixer'
+  gem 'jekyll-minifier'
+  gem 'jekyll-seo-tag'
 end
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,54 @@
+// ---------------------------------------------------------------
+// LOADER
+// ---------------------------------------------------------------
+function loader(){
+  setTimeout(function(){
+    var wrapper = $('.mac-wrapper');
+    var startTimeline = new TimelineMax({onComplete: loaderOff});
+    startTimeline
+    .to("#loader", .6,{x: "100%", ease: Power3.easeInOut}, "startAni")
+    .from(".main-container", .6,{y: "-100%", ease: Power3.easeInOut}, "startAni")
+    .from(".box.side", .6,{y: "100%", ease: Power3.easeInOut}, "startAni")
+  }, 10020);
+}
+
+function loaderOff(){
+  $('#loader').remove();
+}
+
 $(document).ready(function(){
 
-  const easeCustom = [.6,.03,.2,1]
+
+// ---------------------------------------------------------------
+// HTML HISTORY
+// ---------------------------------------------------------------
+
+window.addEventListener('popstate', function(e) {
+  var state = e.state;
+
+  if (state == null || state == "home") {
+    closeSubmenu();
+    navTimeline.reverse();
+  } else {
+    contentSwitchTimeline.add(swichtContent(state))
+    contentSwitchTimeline.play();
+  }
+});
+
+
+
+// ---------------------------------------------------------------
+// MAIN JS
+// ---------------------------------------------------------------
+
+
 
   $(".nav-container").accordion({
       collapsible: true,
       active: 0,
       animate: "swing",
-      event: "click"
+      event: "click",
+      heightStyle: "content"
   });
 
   $("nav h1").click(function(){
@@ -18,7 +60,16 @@ $(document).ready(function(){
   $(document).on("click", ".burger" ,function(){
     $(".burger").toggleClass('is-active');
     $(".burger-menu").toggleClass('is-active');
+    $(".box.side").toggleClass('is-active');
+    $(".main-container").toggleClass('is-active');
   });
+
+  // $(document).on("click", ".imprint-trigger" ,function(){
+  //   $(".imprint").toggleClass('is-active');
+  //   $(".burger-menu").toggle();
+  //   // $(".box.side").toggleClass('is-active');
+  //   // $(".main-container").toggleClass('is-active');
+  // });
 
 
   new TypeIt('.typeItCode', {
@@ -29,11 +80,93 @@ $(document).ready(function(){
      nextStringDelay: ["1000","2000"],
      loop: true,
      loopDelay: "1500"
-
   });
+  new TypeIt('.loadingText', {
+     breakLines: false,
+     autoStart: true,
+     cursorChar: "_",
+     loop: false,
+     loopDelay: "1500"
+  })
+  .type('Loading')
+  .pause(500)
+  .type('.')
+  .pause(500)
+  .type('.')
+  .pause(500)
+  .type('.')
+  .pause(500)
+  .type('.')
+  .pause(500)
+  .type('.')
+  .pause(500)
+  .type('.')
+  .pause(500)
+  .type('.')
+  .pause(500)
+  .type('.')
+  .pause(500)
+  .type('.');
+
+  new TypeIt('.notFoundText', {
+     breakLines: true,
+     autoStart: true,
+     cursorChar: "_",
+     loop: false,
+     loopDelay: "1500"
+  })
+  .type('404 - Datei nicht gefunden - ')
+  .type(' hier geht`s - ')
+  .type('<a href="/" >zurück!</a>')
+
+
+  new TypeIt('.dankeText', {
+     breakLines: true,
+     autoStart: true,
+     cursorChar: "_",
+     loop: false,
+     loopDelay: "1500",
+     speed: 50
+  })
+  .type('Danke - Ihre Nachricht wurde erfolgreicht versendet!')
+  .type(' hier geht`s - ')
+  .type('<a href="/" >zurück!</a>')
+
+
+  /* CONTACT FORM */
+
+  // $('.sendform').click(function() {
+  //     var $contactForm = $('#contact-form');
+  //     console.log($contactForm);
+  //     $contactForm.submit(function(e) {
+  //       console.log(e);
+  //     	e.preventDefault();
+  //     	$.ajax({
+  //     		url: 'https://formspree.io/info@larsknoke.com',
+  //     		method: 'POST',
+  //     		data: $(this).serialize(),
+  //     		dataType: 'json',
+  //     		beforeSend: function() {
+  //     			$contactForm.append('<div class="alert alert--loading">Sending message…</div>');
+  //     		},
+  //     		success: function(data) {
+  //     			$contactForm.find('.alert--loading').hide();
+  //     			$contactForm.append('<div class="alert alert--success">Message sent!</div>');
+  //     		},
+  //     		error: function(err) {
+  //     			$contactForm.find('.alert--loading').hide();
+  //     			$contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
+  //     		}
+  //     	});
+  //     });
+  // });
+
 
 
   /* ANIMATIONS */
+
+
+
 
   function clickedLink() {
     return $(".ui-accordion-header-active").attr("data-link");
@@ -51,6 +184,8 @@ $(document).ready(function(){
   function closeSubmenu() {
     $(".burger-menu").removeClass('is-active');
     $(".burger").removeClass('is-active');
+    $(".box.side").removeClass('is-active');
+    $(".main-container").removeClass('is-active');
   }
 
   var swichtContent = function(section){
@@ -59,59 +194,63 @@ $(document).ready(function(){
     var contentOn = $(".main-content-" + section);
     var anim = new TimelineLite();
     anim
-      .fromTo (contentOn , 0.6, {x: 1000}, {x:"0%",opacity:1,display:'block', ease: Power3.easeInOut})
+      .fromTo (contentOn , 0.6, {x: "100%"}, {x:"0%",opacity:1,display:'block', ease: Power3.easeInOut})
     return anim;
   }
 
   var contentSwitchTimeline = new TimelineMax({paused: true});
+  var navTimeline = new TimelineMax({paused: true, onReverseComplete: clearTimeline});
+
+  function clearTimeline(){
+    navTimeline.clear();
+  }
 
   function navTlGenerator(link){
-    var navTimeline = new TimelineMax({delay: .3, paused: true});
+    var tablet = $(window).width() <= 768;
+    var mobile = $(window).width() <= 667;
+    var navTimeline = new TimelineMax();
     navTimeline
+      .to(".footer", .4, {opacity: 0, ease: Power3.easeInOut}, "switch")
       .to(".nav-container", .6,{x: "-100%", ease: Power3.easeInOut}, "switch")
-      .to(".main-start", .6,{x: "-100%", opacity: 0, ease: Power3.easeInOut}, "switch+=0.3")
-      .to(".main-container", .6,{width: "75%", ease: Power3.easeInOut}, "switch+=0.3")
+      .to(".main-start", .6,{x: "-100%",height: 0, opacity: 0, ease: Power3.easeInOut}, "switch+=0.3")
+      .set(".main-content", { css: { position: mobile ? "relative" : "absolute" } })
+      .to(".main-container", .6,{width: (tablet ? "100%" : "75%"), ease: Power3.easeInOut}, "switch+=0.3")
+      .to(".box.side", .6,{width: (tablet ? "0%" : "25%"), ease: Power3.easeInOut}, "switch+=0.3")
       .to(link, .6,{opacity: 1, x: "0%", ease: Power3.easeInOut}, "switch+=0.3")
       .to(".start-side", .6,{opacity: 1, x: "100%", ease: Power3.easeInOut}, "switch+=0.3")
       .to(".main-content", .6,{opacity: 1, x: "0%", ease: Power3.easeInOut}, "switch+=0.3")
       .to(".burger", .2, {opacity: 1, x: 20})
+      .to(".arrow-back", .4, {width: "30px", marginRight: "10px"})
+      .to(".footer", .4, {opacity: 1, bottom: mobile ? "1rem" : "2rem"})
     return navTimeline
   }
 
-  function backToHome(link){
-    var backToHomeTl = new TimelineMax({pause: true})
-    backToHomeTl
-      .to(".nav-container", .6,{x: "0%", ease: Power3.easeInOut}, "switch")
-      .to(".main-start", .6,{x: "0%", opacity: 1, ease: Power3.easeInOut}, "switch+=0.3")
-      .to(".main-container", .6,{width: "60%", ease: Power3.easeInOut}, "switch+=0.3")
-      .to(link, .6,{ x: "-100%", ease: Power3.easeInOut}, "switch+=0.3")
-      .to(".start-side", .6,{opacity: 1, x: "0%", ease: Power3.easeInOut}, "switch+=0.3")
-      .to(".main-content", .6,{opacity: 0, x: "100%", ease: Power3.easeInOut}, "switch+=0.3")
-      .to(".burger", .2, {opacity: 0, x: 0})
-    return backToHomeTl
-  }
-
-
-  $(".arrow").click(function(){
+  $(".arrow, .arrow2, .imprint-trigger").click(function(){
+    history.pushState(clickedLink(), null, clickedLink());
     showContent();
     var link = $("." + clickedLink() + "-side");
-    var tl = navTlGenerator(link);
-    tl.play();
+    navTimeline.add(navTlGenerator(link))
+    navTimeline.play();
   });
 
-  $(".logo").click(function(){
+  $(".logo, .arrow-back").click(function(){
+    history.pushState("home", null, "home");
     closeSubmenu();
-    var link = $("." + clickedLink() + "-side");
-    var tl = backToHome(link);
-    tl.play();
+    navTimeline.reverse();
   });
+
 
   $(".toSection").click(function(event){
     event.preventDefault();
+    closeSubmenu();
     var section = $(this).attr("data-link");
-    console.log(section);
+    history.pushState(section, null, section);
     contentSwitchTimeline.add(swichtContent(section))
     contentSwitchTimeline.play();
+  });
+
+  $(document).on("click", ".closeMenu" ,function(){
+    closeSubmenu();
   });
 
 });
